ff61212b9342b381f9fba3984228edda08dad18c
Vladislav Chernyshev <tchernyshev.vladik@yandex.ru>
2025-03-22 15:13:02 +0300

    v.0.0.0.2

--- app/handlers/start_router.py
+++ app/handlers/start_router.py
@@ -1,9 +1,19 @@
 from aiogram import Router, F
-from aiogram.filters import CommandStart,Command
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
+
+from app.keyboards.all_kb import main_kb, create_spec_kb
 
 start_router = Router()
 
-@start_router.message(Commadstart())
-async def cmd_start(messange: Message):
-    await messange
+@start_router.message(CommandStart())
+async def cmd_start(message:Message):
+    await message.answer('запуск сообщения по' + \
+        'команде /start используя фильтр CommandStart()',
+        reply_markup=main_kb(message.from_user.id))
+    
+@start_router.message(Command('start_2'))
+async def cmd_start_2(message: Message):
+    await message.answer('Запуск сообщения по' + \
+        ' комманде /start_2 используя фильтр CommandStart()',
+        reply_markup=create_spec_kb())

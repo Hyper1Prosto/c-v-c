7fad6d71f11c00a58ead27e90bfcb9c1da4ad4db
Max <you@example.com>
2025-04-12 16:33:56 +0300

    v42

---
+++ app/db_hendler/db_class.py
@@ -0,0 +1,35 @@
+import sqlite3
+import datetime
+def create_connection():
+    connection = None
+    try:
+        connection = sqlite3.connect('database.db')
+        print("Подключение произошло успешно!")
+    except sqlite3.Error as e:
+        print(f"Ошибка: {e}")
+    return connection
+
+
+def crete_user(user_id: int,
+               full_name: str):
+    conn = create_connection()
+    cursor = conn.cursor()
+    
+    find_user = """SELECT * FROM users WHERE user_id = ?"""
+    
+    cursor.execute(find_user, (user_id))
+    user = cursor.fetchone()
+    
+    if user:
+       print("Пользовательть уже существуе ")
+       return False
+    create_at = datetime.datetime.now()
+    update_at = datetime.datetime.now()
+    balance = 0
+    
+    create_user = """INSERT INTO users (user_id, fullname, balance, create_at, update_at)
+    VALUES (?, ?, ?, ?, ?)"""
+    cursor.execute(create_user, (user_id, full_name, balance, create_at, update_at))
+    
+    conn.commit()
+    return True
